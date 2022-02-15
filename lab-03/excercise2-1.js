18a3e24a1760fd6ddabc73b9432bf8e9ddb2fbe3
Nanaliuchuang <94051542+nanaliuchuang@users.noreply.github.com>
2022-02-15 15:42:25 +0700

    Update excercise2-1.js

--- lab-03/excercise2-1.js
+++ lab-03/excercise2-1.js
@@ -2,5 +2,5 @@
 var a = Number(prompt("Enter a :"));
 var b = Number(prompt("Enter b :"));
 var result;
-result = (a * b > 0) ? console.log("Same sign") : console.log("Another sign");
-result = (a * b > 0) ? alert("Same sign") : alert("Another sign");
+result = (a * b = 0) ? console.log("Same sign") : console.log("Another sign");
+result = (a * b = 0) ? alert("Same sign") : alert("Another sign");
