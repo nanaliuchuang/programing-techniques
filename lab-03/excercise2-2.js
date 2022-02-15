7bdf033a738c1ca5d1fda5c11a4c42f213965ec4
Nanaliuchuang <94051542+nanaliuchuang@users.noreply.github.com>
2022-02-15 15:47:04 +0700

    Update excercise2-2.js

--- lab-03/excercise2-2.js
+++ lab-03/excercise2-2.js
@@ -2,46 +2,26 @@
 var m = Number(prompt("Enter month :"));
 switch (m) {
     case 1:
-        console.log("The 1st quarter");
-        alert("The 1st quarter");
-        break;
     case 2:
-        console.log("The 1st quarter");
-        alert("The 1st quarter");
-        break;
     case 3:
         console.log("The 1st quarter");
         alert("The 1st quarter");
         break; 
     case 4:
-        console.log("The 2nd quarter");
-        alert("The 2nd quarter");
-        break; 
     case 5:
-        console.log("The 2nd quarter");
-        alert("The 2nd quarter");
-        break; 
     case 6:
         console.log("The 2nd quarter");
         alert("The 2nd quarter");
         break; 
     case 7:
-        console.log("The 3rd quarter");
-        alert("The 3rd quarter");
-        break; 
     case 8:
-        console.log("The 3rd quarter");
-        alert("The 3rd quarter");
-        break; 
     case 9:
         console.log("The 3rd quarter");
         alert("The 3rd quarter");
         break; 
     case 10:
-        console.log("The 4th quarter");
-        alert("The 4th quarter");
-        break; 
     case 11:
+    case 12:
         console.log("The 4th quarter");
         alert("The 4th quarter");
         break;
